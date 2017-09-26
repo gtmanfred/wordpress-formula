46823f1249c8ef5171e68a6f3a4aefc644580941
Daniel Wallace <danielwallace@gtmanfred.com>
2017-09-26 10:04:55 -0600

    fix tests

--- tests/integration/test_wordpress.py
+++ tests/integration/test_wordpress.py
@@ -2,4 +2,4 @@ import requests
 
 
 def test_server(host_ip, http_port):
-    assert requests.get(f'http://{host_ip}:{http_port}', headers={'Host': 'blog.manfred.io'})
+    assert requests.get(f'http://{host_ip}:{http_port}', headers={'Host': 'blog.manfred.io'}).status_code == 200
