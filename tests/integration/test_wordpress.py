76f2dc67261a2a28bc48906efab3e06dd9ed4d7e
Daniel Wallace <danielwallace@gtmanfred.com>
2017-09-26 14:02:24 -0600

    host_ip not http_ip

--- tests/integration/test_wordpress.py
+++ tests/integration/test_wordpress.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_server(host_ip, http_port):
-    resp = requests.get('http://{0}:{1}'.format(http_ip, http_port), headers={'Host': 'blog.manfred.io'})
+    resp = requests.get('http://{0}:{1}'.format(host_ip, http_port), headers={'Host': 'blog.manfred.io'})
     assert resp.status_code == 200
