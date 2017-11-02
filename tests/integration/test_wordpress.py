b574168b6c562ef666080bcb59333322621f5178
Daniel Wallace <danielwallace@gtmanfred.com>
2017-11-02 10:48:59 -0600

    add test url

--- tests/integration/test_wordpress.py
+++ tests/integration/test_wordpress.py
@@ -4,3 +4,8 @@ import requests
 def test_server(host_ip, http_port):
     resp = requests.get('http://{0}:{1}'.format(host_ip, http_port), headers={'Host': 'blog.manfred.io'})
     assert resp.status_code == 200
+
+
+def test_wordpress_url(host_ip, http_port):
+    resp = requests.get('http://{0}:{1}'.format(host_ip, http_port), headers={'Host': 'blog.manfred.io'})
+    assert 'blog.manfred.io' in resp.text
